ebc68fe2455a9a8d012413c56bf57cb2390d94df
Pablo Deeleman <deeleman@gmail.com>
2019-02-03 21:51:18 +0100

    HTML generation simplified for nav selector listbox

--- src/Calendar/CalendarNavSelector.js
+++ src/Calendar/CalendarNavSelector.js
@@ -2,19 +2,12 @@ import React from 'react';
 import { monthNames } from './i18n/en-gb';
 
 export default function CalendarNavSelector(props) {
-  const renderMonthOptions = [];
-
-  for (let monthIndex = 0; monthIndex < 12; monthIndex++) {
-    renderMonthOptions.push(
-      <option key={monthIndex} value={monthIndex}>{monthNames[monthIndex]} {props.year}</option>
-    );
-  }
-
+  const monthIndexes = new Array(12).fill(0).map((value, index) => value + index);
   const onChange = (event) => props.onChange(parseInt(event.target.value, 10), event)
 
   return (
     <select className="nav__months-select" value={props.month} onChange={onChange}>
-      {renderMonthOptions}
+      {monthIndexes.map(monthIndex => <option key={monthIndex} value={monthIndex}>{monthNames[monthIndex]} {props.year}</option>)}
     </select>
   );
 }
